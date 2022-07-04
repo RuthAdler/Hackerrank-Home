be6c1f4006223e3bbcab6446fb551994713848b2
RuthAdler <85292214+RuthAdler@users.noreply.github.com>
2022-07-04 13:32:01 +0300

    Create Simple Array Sum.py
    Given an array of integers, find the sum of its elements.
    
    For example, if the array , , so return .
    
    Function Description
    
    Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
    
    simpleArraySum has the following parameter(s):
    
    ar: an array of integers
    Input Format
    
    The first line contains an integer, , denoting the size of the array.
    The second line contains  space-separated integers representing the array's elements.

---
+++ Simple Array Sum.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    c = 0
+    for i in ar:
+        c += i
+    return (c)
