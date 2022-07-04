cff93aaa571f879b7b0b1bdec983ffc62fec914b
RuthAdler <85292214+RuthAdler@users.noreply.github.com>
2022-07-04 14:26:22 +0300

    Create A Very Big Sum.py
    n this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
    
    Function Description
    
    Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
    
    aVeryBigSum has the following parameter(s):
    
    int ar[n]: an array of integers .
    Return
    
    long: the sum of all array elements
    Input Format
    
    The first line of the input consists of an integer .
    The next line contains  space-separated integers contained in the array.
    
    Output Format
    
    Return the integer sum of the elements in the array.

---
+++ A Very Big Sum.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    output = 0
+    for i in ar:
+        output +=i   
+    return(output)
